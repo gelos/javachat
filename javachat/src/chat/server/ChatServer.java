17d714ecbfd523479c708bc9a3a3154c7b9fe010
gelos <9svtcrze9b2m@mail.ru>
2018-01-17 15:39:08 +0300

    Refactoring ChatServer code for process console input in distinct thread. Write JUnit test for server start/stop.

--- javachat/src/chat/server/ChatServer.java
+++ javachat/src/chat/server/ChatServer.java
@@ -15,6 +15,10 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * 
  * @see ChatHandler
  */
+/**
+ * @author Oleg.Olyunin
+ *
+ */
 public class ChatServer {
 
   /** The server port number. */
@@ -23,7 +27,6 @@ public class ChatServer {
   /** The server host ip address. */
   public final static String SERVER_IP = "127.0.0.1";
 
-
   /** The server socket. */
   private ServerSocket serverSocket;
 
@@ -31,43 +34,65 @@ public class ChatServer {
   Socket clientConnection;
 
   /** The client session handlers thread-safe storage. */
-  // ArrayList handlers;
   CopyOnWriteArrayList<ChatHandler> chatHandlers;
 
   /** The distinct thread to accept client connections. */
-  private Thread acception = new Thread("Acception Thread") {
+  // private Thread chatClientThread = new Thread("Chat Client chatClientThread thread.") {
+  private Thread chatClientThread = new Thread(new Runnable() {
 
-    //private final AtomicBoolean running = new AtomicBoolean(false);
-    
     @Override
     public void run() {
-
+      // TODO Auto-generated method stub
       try {
         while (true) {
+
           System.out.println("Waiting for client connection...");
-          // new EchoServer2(serverSocket.accept());
 
           // Accept client connection and return new client socket
           clientConnection = serverSocket.accept();
 
           String ip = (((InetSocketAddress) clientConnection.getRemoteSocketAddress()).getAddress())
               .toString().replace("/", "");
-
           System.out.println("Accept client connection from " + ip);
 
           // Create new ChatHandler with existing client handlers and new client socket as thread
           new ChatHandler(clientConnection, chatHandlers).start();
         }
-      } catch (SocketException e) {
-        if (serverSocket == null || serverSocket.isClosed())
+
+      } catch (SocketException e) { // Catch SocketException to avoid error while closing server
+        if (serverSocket == null || serverSocket.isClosed()) {
           System.out.println("Server stoped.");
-      } catch (IOException e) {
+        }
+
+      } catch (IOException e) { // Exit program on IOException
         System.err.println("Accept failed.");
         System.exit(1);
       }
-
     }
-  };
+  });
+
+  /** The process console input. */
+  private Thread processConsoleInput = new Thread(new Runnable() {
+
+    @Override
+    public void run() {
+
+      Scanner consoleInput = new Scanner(System.in);
+      String s = consoleInput.next();
+
+      while (true) {
+
+        // on quit command stop server
+        if (s.equalsIgnoreCase("quit")) {
+          break;
+        }
+        s = consoleInput.next();
+
+      }
+      close();
+    }
+  });
+
 
   /**
    * Instantiates a new chat server.
@@ -77,89 +102,56 @@ public class ChatServer {
 
       System.out.println("Chat server starting...");
 
-      // Init client session handlers storage
-      // handlers = new ArrayList();
+      // Initialize client session handlers storage
       chatHandlers = new CopyOnWriteArrayList<ChatHandler>();
 
-      // Init server socket with _SERVER_SOCKET port
+      // Initialize server socket with _SERVER_SOCKET port
       serverSocket = new ServerSocket(SERVER_PORT);
 
       System.out.println("Connection socket on port " + SERVER_PORT + " created.");
-      acception.start();
+      chatClientThread.start();
 
-      /*
-       * new Thread() {
-       * 
-       * @Override public void run() { System.out.println("Type exit to exit;-)"); Console c =
-       * System.console(); String msg = c.readLine(); if (msg.equals("exit")) { // some cleaning up
-       * code... System.exit(0); } } }.start();
-       * 
-       */
+    } catch (IOException ioe) {
 
-
-      /*
-       * Scanner in = new Scanner(System.in);
-       * 
-       * int i = in.nextInt(); String s = in.next();
-       */
-
-
-      // Wait for client connection
-      // for (;;) {
-      // System.in
-      // while (in.next .next())
-
-      // Accept client connection and return new client socket
-      // s = serverSocket.accept();
-
-      // Create new ChatHandler with existing client handlers and new client socket as thread
-      // new ChatHandler(s, handlers).start();
-
-
-      // }
-    } catch (
-
-    IOException ioe) {
       System.out.println(ioe.getMessage());
 
     }
 
     // support to close, using the command line.
-
     System.out.println("Server started, type quit to shutdown server: ");
 
-    Scanner scn = new Scanner(System.in);
-    String s = scn.next();
-
-    while (true) {
-      if (s.equalsIgnoreCase("quit")) {
-        close();
-        break;
-      }
-      s = scn.next();
-    }
+    // start thread to process console command input
+    processConsoleInput.start();
 
   }
 
+  /**
+   * Close.
+   *
+   * @return true, if successful
+   */
   public boolean close() {
     try {
-      
-      System.out.println("Stopping acception thread...");
-      //acception.sto
-      
+
+      System.out.println("Stopping chatClientThread thread...");
+
       System.out.println("Closing all client handlers...");
+      
+      // Close all ChatHadnlers
       for (ChatHandler ch : chatHandlers) {
         if (!ch.close()) {
           System.out.println("Client handlers closing error.");
         };
       }
-            
+
       System.out.println("Stoping server...");
       serverSocket.close();
       serverSocket = null;
     } catch (IOException e) {
       System.err.println("Could not close port: " + SERVER_PORT + ".");
       // System.exit(1);
+      
+      // TODO process return code
       return false;
     }
 
@@ -174,7 +166,7 @@ public class ChatServer {
    * @param args the arguments
    */
   public static void main(String[] args) {
-    ChatServer tes = new ChatServer();
+    new ChatServer();
   }
 }
 
--- javachat/src/chat/test/ChatTest.java
+++ javachat/src/chat/test/ChatTest.java
@@ -1,52 +1,108 @@
 package chat.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import chat.client.mvp.swing.ChatClientSwingPresenter;
 import chat.server.ChatServer;
 
 class ChatTest {
 
-  ChatServer server = null; 
-  
+  //ChatServer server = null;
+  AtomicReference<ChatServer> server = new AtomicReference<ChatServer>(null);
+  AtomicInteger number = new AtomicInteger(0);
+
   @BeforeEach
   void setUp() throws Exception {}
 
   @AfterEach
   void tearDown() throws Exception {}
 
+  @Disabled
   @Test
-  void testServerStartStop() {
-        
-    server = null;
-    
-    Thread chatServerThread = new Thread() {
-      @Override
+  void testCreateChatServer() {
+    Thread th = new Thread(new Runnable() {
+      public ChatServer server = null;
+            
       public void run() {
         server = new ChatServer();
       }
-      
+    });
+    th.start();
+    
+    //Thread.sleep(5000);
+    
+    //assertNotNull(th. .server);
+  }
+  
+  
+  //@Disabled
+  @Test
+  void testServerStartStop() throws Throwable {
+
+    final AtomicReference<Throwable> exception = new AtomicReference<>();
+    Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+      @Override
+      public void uncaughtException(final Thread t, final Throwable e) {
+        exception.compareAndSet(null, e);
+      }
+    });
+
+    //server = null;
+
+    Thread chatServerThread = new Thread() {
+      @Override
+      public void run() {
+        number.set(number.get()+1);
+        //number.incrementAndGet();
+        //assertNotNull(server.get(), "after start in thread");
+        assertEquals(1, number.get());
+        //System.out.println("Start thread");
+        
+        //server = new ChatServer();
+        server.set(new ChatServer());
+        System.out.println("Start thread");
+        assertNotNull(server.get(), "after start in thread");
+        
+        
+      }
+
     };
-    
-    //ChatServer server = new ChatServer();
-    
+
+    // ChatServer server = new ChatServer();
+
+    //assertEquals(0, number);
+    assertNull(server.get(), "before start");
     chatServerThread.start();
+    assertNull(server.get(), "after start");
+    //number += 1;
+    assertEquals(0, number.get());
+   
     try {
       Thread.sleep(3000);
     } catch (InterruptedException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
+    } 
+    assertNotNull(server.get(), "after timeout");
+    assertEquals(1, number.get(), "after timeout");
+    //assertNull(server.get());
+    assertTrue(server.get().close());
+    // ChatClientSwingPresenter client = new ChatClientSwingPresenter();
+
+    // fail("Not yet implemented");
+
+    if (exception.get() != null) {
+      throw exception.get();
     }
-    assertNull(server);
-    //assertTrue(server.close());
-    //ChatClientSwingPresenter client = new ChatClientSwingPresenter();
-    
-    //fail("Not yet implemented");
+
   }
 
 }
